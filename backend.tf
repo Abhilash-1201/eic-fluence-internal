2fc1e383513c76aed3d348a0b34648700b5954b7
rajarammohan0203 <52132926+rajarammohan0203@users.noreply.github.com>
2024-04-16 08:25:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "demo-fluence-terraform-state"
+    bucket = "demo-fluence-terraform"
     key = "fluence/terraform.tfstate"
     region = "us-east-2" #ohio
     dynamodb_table = "demo-fluence-state-locking"
